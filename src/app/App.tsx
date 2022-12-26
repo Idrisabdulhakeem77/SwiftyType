66f6738d122cda0fe49188f98be4c72b7dacff83
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-26 20:35:01 +0100

    added a list of themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,23 +2,40 @@ import { useAppDispatch, useAppSelector } from "./hooks";
 import { ThemeProvider } from "styled-components";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Home } from "../pages";
+import { useCallback } from "react";
 
 const App = () => {
   const dispatch = useAppDispatch();
   const location = useLocation();
 
   const { theme, commandLine } = useAppSelector(({ app }) => app);
+  const config = useAppSelector(({ config }) => config);
 
+  const {
+    quickRestart,
+    language,
+    fontFamily,
+    pageWidth,
+    transitionSpeed,
+    themeName,
+    randomTheme,
+    favoriteThemes,
+  } = config;
 
+  const setRandomTheme = useCallback(
+     async() => {
+       if(randomTheme === "off") return
+      //  let filteredThemes = 
+     },
+    [ ],
+  )
 
-  const setRandomTheme = () => {
-     
-  }
- 
   return (
     <ThemeProvider theme={{ ...theme }}>
       <Routes location={location} key={location.pathname}>
-         <Route path="/" element={<Home setRandomTheme={setRandomTheme}/>}>  </Route>
+        <Route path="/" element={<Home setRandomTheme={setRandomTheme} />}>
+          
+        </Route>
       </Routes>
     </ThemeProvider>
   );
