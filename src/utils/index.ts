263850cb6758b283d73da6bf4d60a2a3e3c55721
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-29 08:48:44 +0100

    added kogasa.ts utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,5 @@
 export { default as mean } from './mean'
 export { default as StandardDeviation } from './standardDeviation'
+export { default as coefficientOfVariation } from './coefficientOfVariation'
+export { default as shuffleArray } from './shuffleArray'
+export { default as kogasa } from './kogasa'
---
+++ src/utils/kogasa.ts
@@ -0,0 +1,10 @@
+import coefficientOfVariation from "./coefficientOfVariation";
+
+
+const kogasa = (arr: number[]): number => {
+    const cov = coefficientOfVariation(arr);
+    return 100 * (1 - Math.tanh(cov + Math.pow(cov, 3) / 3 + Math.pow(cov, 5) / 5))
+
+}
+
+export default kogasa
